f217117c2e218b6bc858e05e1a7736ab31f56f41
Yunuo Xiong <38746432+xiongyunuo@users.noreply.github.com>
2020-03-18 20:59:49 +0800

    Add files via upload

--- event_script.js
+++ event_script.js
@@ -4,6 +4,24 @@ let message2 = "Get Text";
 let message3 = "Text";
 let message4 = "Save";
 let currentText = "I-I_I-I SUCK";
+let keys = ["number", "deviation", "overlay", "double", "space", "english", "chaotic", "upper", "text"];
+let defaults = [10, 0, 0.5, 0.5, false, true, true, 0.5, "I-I_I-I SUCK"];
+
+for (let i = 0; i < keys.length; i++) {
+  chrome.storage.sync.get(keys[i], items => {
+    if (items[keys[i]] === undefined) {
+      let items = {};
+      items[keys[i]] = defaults[i];
+      chrome.storage.sync.set(items, () => {});
+    }
+  });
+}
+
+chrome.storage.sync.get(keys[8], items => {
+  if (items[keys[8]] !== undefined) {
+    currentText = items[keys[8]];
+  }
+});
 
 chrome.commands.onCommand.addListener(command => {
   if (command == copyCommand) {
@@ -19,18 +37,8 @@ chrome.runtime.onMessage.addListener((msg, _, response) => {
   else if (msg.message == message4) {
     response();
     currentText = msg.text;
+    let items = {};
+    items[keys[8]] = currentText;
+    chrome.storage.sync.set(items, () => {});
   }
 });
-
-let keys = ["number", "deviation", "overlay", "double", "space", "english", "chaotic", "upper"];
-let defaults = [10, 0, 0.5, 0.5, false, true, true, 0.5];
-
-for (let i = 0; i < keys.length; i++) {
-  chrome.storage.sync.get(keys[i], items => {
-    if (items[keys[i]] === undefined) {
-      let items = {};
-      items[keys[i]] = defaults[i];
-      chrome.storage.sync.set(items, () => {});
-    }
-  });
-}
